dc7ec9956f0b16f056b15194719af1a12da3c4ac
baldx <mr.ahmad.m2006@gmail.com>
2023-06-03 11:51:27 +0200

    add event for read btn

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let myLibrary = [];
 
+
+let readBtn = document.querySelectorAll(".readBtn");
+
+readBtn.forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.toggle("not-read")
+
+        if (element.classList.contains("not-read")) {
+            element.textContent = "Not Read";
+        }
+        else element.textContent = "Read";
+    })
+});
+
 function Book() {
     //constructor
 }
@@ -7,3 +21,4 @@ function Book() {
 function addBookToLibrary() {
     //do shit here
 }
+
