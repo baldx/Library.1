841f7dc304d6be13c55a216d23c729d9ac561f65
baldx <mr.ahmad.m2006@gmail.com>
2023-06-04 12:57:15 +0200

    add book constructor & read value

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let myLibrary = [1, 2, 3, 4];
+let myLibrary = [
+    {
+        title: "Abdi",
+        author: "Mokandez den tredje",
+        pages: 420,
+        read: true
+    }
+];
 
 
 let readBtn = document.querySelectorAll(".readBtn");
 let addBookBtn = document.querySelector(".addBook");
 let removeBook = document.querySelectorAll(".remove");
 let library = document.querySelector(".library");
+
+
+let form = document.querySelector("form");
+let titleInput = form.querySelector("#title");
+let authorInput = form.querySelector("#author");
+let pagesInput = form.querySelector("#number");
+let submitBook = form.querySelector("#submit");
 
 function Book(title, author, pages) {
     this.title = title;
@@ -13,7 +27,17 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
-    //do shit here
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = getReadValue();
+    let newBook = new Book(title, author, pages);
+    library.push(newBook);
+}
+
+const getReadValue = () => {
+    if (form.querySelector('input[name="read"]:checked').value == "yes") return true;
+    else return false;
 }
 
 readBtn.forEach(element => {
@@ -30,16 +54,29 @@ readBtn.forEach(element => {
 addBookBtn.onclick = () => {
     library.innerHTML += `
     <form>
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title" required maxlength="50">
-        <label for="author">Author</label>
-        <input type="text" name="author" id="author" required maxlength="50">
-        <label for="pages">Pages</label>
-        <input type="number" name="number" id="number" required min="0">
-        <button type="submit">Add book</button>
-    </form>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" required maxlength="50">
+            <label for="author">Author</label>
+            <input type="text" name="author" id="author" required maxlength="50">
+            <label for="pages">Pages</label>
+            <input type="number" name="number" id="number" required min="0">
+            <fieldset class="radiogroup">
+                <legend>Read book?</legend>
+                <label for="yes">
+                    <input type="radio" name="yes" id="yes" value="yes"> Yes
+                </label>
+                <label for="no">
+                    <input type="radio" name="no" id="no" value="no"> No
+                </label>
+            </fieldset>
+            <button type="submit" id="submit">Add book</button>
+        </form>
     `
 }
+
+submitBook.addEventListener("click", element => {
+    element.preventDefault();
+})
 
 removeBook.forEach(element => {
     element.addEventListener("click", () => {
